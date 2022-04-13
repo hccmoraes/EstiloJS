fbfb861fdc9892cf30bf5bcaa68cbfbf9d655830
henriqueccmoraes <henriqueccmoraes@fac.pe.senac.br>
2022-04-12 22:25:26 -0300

    Inserindo o código no js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert(`Olá!`)
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12){
+        // Bom dia
+        img.src = 'manha1.jpg'
+    } else if(hora >= 12 && hora < 18){
+        // Boa tarde
+        img.src = 'tarde1.jpg'
+    } else {
+        // Boa noite
+        img.src = 'noite1.jpg'
+    }
+}
