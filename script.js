dbad4606eef5599d60db6beb1096007b876ad037
henriqueccmoraes <henriqueccmoraes@fac.pe.senac.br>
2022-04-12 22:40:01 -0300

    Finalizando o css

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    
     msg.innerHTML = `Agora são ${hora} horas.`
     if(hora >= 0 && hora < 12){
         // Bom dia
         img.src = 'manha1.jpg'
+        document.body.style.background = '#e2cd9f'
     } else if(hora >= 12 && hora < 18){
         // Boa tarde
         img.src = 'tarde1.jpg'
+        document.body.style.background = '#b9846f'
     } else {
         // Boa noite
         img.src = 'noite1.jpg'
+        document.body.style.background = '#515154'
     }
 }
